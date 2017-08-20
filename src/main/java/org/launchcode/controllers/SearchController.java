778b99694d511bcd59b2609a430b6f24d6b4c85f
Wren Bareilles <wren.ellbee@gmail.com>
2017-08-20 16:56:17 -0700

    Completed main studio objectives. To do: hide results count upon first load of the page.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,4 +24,52 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value = "results")
+    public String searchByColumn(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        /**
+         * The all-search is a copy-paste from TechJobs Console.
+         * Differences: the ArrayList is built using a public method from JobData called findAll()
+         */
+
+        ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+        ArrayList<HashMap<String, String>> found_jobs = new ArrayList<>();
+
+        searchTerm = searchTerm.toLowerCase();
+
+        if (searchType.equals("all")) {
+
+            for (HashMap<String, String> row: jobs) {
+                for (String column : row.keySet()) {
+
+                    String aValue = row.get(column);
+                    aValue = aValue.toLowerCase();
+
+                    if (aValue.contains(searchTerm) && !found_jobs.contains(row)) {
+                        found_jobs.add(row);
+                    }
+                }
+
+            }
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("displayJobs", found_jobs);
+            return "search";
+        } else {
+
+            for (HashMap<String, String> row : jobs) {
+
+                String aValue = row.get(searchType);
+                aValue = aValue.toLowerCase();
+
+                if (aValue.contains(searchTerm)) {
+                    found_jobs.add(row);
+                }
+            }
+
+
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("displayJobs", found_jobs);
+            return "search";
+        }
+    }
+
 }
